a5981b54295d31ea188612d339cffa5b6e76c9f2
caopei <cp17764029185@163.com>
2020-01-26 09:53:00 +0800

    将文件添加到本地仓库

---
+++ mybatisspringboot2/src/main/java/com/caopei/Mybatisspringboot1Application.java
@@ -0,0 +1,13 @@
+package com.caopei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatisspringboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatisspringboot1Application.class, args);
+    }
+
+}
---
+++ mybatisspringboot2/src/test/java/com/caopei/Mybatisspringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caopei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatisspringboot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
